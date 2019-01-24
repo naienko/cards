34d846a3ac7b09e4552ed6308b0ffbe62a518219
Panya Farnette <naienko@gmail.com>
2019-01-23 19:04:37 -0600

    it works but it's clunky

--- cards.js
+++ cards.js
@@ -7,27 +7,31 @@ document.querySelector("#cardFactory").addEventListener("click", function() {
     let cardText = textarea.value;
     cardNumber++;
     cardFactory(cardText);
+    addDeleteEvent();
 })
 
 const cardFactory = (cardText) => {
     const newCard = document.createElement("div");
-    newCard.innerHTML = `${cardText} <div><button id="remove--${cardNumber}">delete</div>`;
-    newCard.id = `"card--${cardNumber}"`;
+    newCard.innerHTML = `<div class="cardText">${cardText}</div> <div><button id="remove--${cardNumber}">delete</div>`;
+    newCard.id = `card--${cardNumber}`;
     newCard.setAttribute("class", "card")
     container.appendChild(newCard);
 }
 
-const deleteButtons = container.querySelectorAll("button");
-
-deleteButtons.forEach(element => {
-    element.addEventListener("click", function() {
-        let chosenCard = element.parentNode.nodeName;
-        container.removeChild(chosenCard);
-    })
-});
-
-// document.querySelector(`#remove--${cardNumber}`).addEventListener("click", function() {
-//     let chosenCard = document.querySelector(`#card--${cardNumber}`);
-//     console.log(chosenCard);
-//     container.removeChild(chosenCard);
-// })
+const addDeleteEvent = () => {
+    const deleteButtons = container.querySelectorAll("button");
+    const cardList = container.querySelectorAll(".card");
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const element = deleteButtons[i];
+        element.addEventListener("click", function() {
+            let cardID = "";
+            let delNumber = event.target.id.split("--")[1];
+            cardList.forEach(element => {
+                if (element.id === `card--${delNumber}`) {
+                    cardID = element.id;
+                }
+            });
+            container.removeChild(container.querySelector(`#${cardID}`));
+        })
+    }
+}
